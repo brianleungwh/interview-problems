41fa41d3d362f24db37cf50815052bda6c4fbf8c
brianleung <bleungwh@gmail.com>
2016-01-06 18:59:54 -0800

    Implement binary search in python.

---
+++ python/binarySearch/binary_search.py
@@ -0,0 +1,19 @@
+import math
+
+
+# binary_search([1, 2, 3, 4, 5], 4) // 3
+# binary_search([1, 2, 3, 4, 5], 10) // None
+def binary_search(sorted_list, target):
+    def search(sorted_list, target, low, high):
+        if low >= high:
+            return
+        mid_index = ((high - low) / 2) + low
+        mid_item = sorted_list[mid_index]
+        if target > mid_item:
+            return search(sorted_list, target, mid_index, high)
+        elif target < mid_item:
+            return search(sorted_list, target, low, mid_index)
+        else:
+            # print(mid_index)
+            return mid_index
+    return search(sorted_list, target, 0, len(sorted_list))
